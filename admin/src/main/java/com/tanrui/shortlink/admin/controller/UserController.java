cb36644c9dfcd3c63af63de0c90a14a6112cc576
tan rui <tan870856@gamil.com>
2024-11-02 09:27:17 +0800

    feature:开发全局异常拦截器之后，对于UserController的优化及解释

--- admin/src/main/java/com/tanrui/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/tanrui/shortlink/admin/controller/UserController.java
@@ -3,7 +3,6 @@ package com.tanrui.shortlink.admin.controller;
 
 import com.tanrui.shortlink.admin.common.convention.result.Result;
 import com.tanrui.shortlink.admin.common.convention.result.Results;
-import com.tanrui.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.tanrui.shortlink.admin.dto.resp.UserRespDTO;
 import com.tanrui.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -27,11 +26,19 @@ public class UserController {
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
-        UserRespDTO userRespDTO = userService.getUserByUsername(username);
-            if (userRespDTO == null) {
-                return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-            }
-                return Results.success(userRespDTO);
+        /**
+         *     开发全局异常拦截器之后不需要, 直接return success即可，因为userRespDTO为null的话，
+         *     已经在UserServiceImpl中抛出ClientException(UserErrorCodeEnum.USER_NULL)，被全局拦截器拦截了，所以不需要进行判断
+         *     userRespDTO是否为null，如果进行到这里的话，userRespDTO必不为空
+         *
+         *     UserRespDTO userRespDTO = userService.getUserByUsername(username);
+         *     if (userRespDTO == null) {
+         *         return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
+         *     }
+         *         return Results.success(userRespDTO);
+         * }
+         */
+        return Results.success(userService.getUserByUsername(username));
     }
 }
 
