c2c36e8d08c9a2813abcd4dcddb07b114fa065b1
tan rui <tan870856@gamil.com>
2024-11-02 11:35:23 +0800

    feature:对手机号使用自定义序列化类

--- admin/src/main/java/com/tanrui/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/tanrui/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,5 +1,7 @@
 package com.tanrui.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.tanrui.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -10,7 +12,14 @@ public class UserRespDTO {
     private Long id;
     private String username;
     private String realName;
+
+    /**
+     * 手机号
+     */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
+
+
     private String mail;
 
 }
