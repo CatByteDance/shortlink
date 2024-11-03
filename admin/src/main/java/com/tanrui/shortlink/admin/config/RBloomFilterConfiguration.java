bfa2a3aa8c9f9677db28a1fd5c0b551bbef843af
tan rui <tan870856@gamil.com>
2024-11-03 09:25:28 +0800

    feature:布隆过滤器类(防止用户注册查询数据库)

---
+++ admin/src/main/java/com/tanrui/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -0,0 +1,25 @@
+package com.tanrui.shortlink.admin.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+
+//      返回初始化后的布隆过滤器对象。这个对象将被 Spring 容器管理，并可以在其他地方通过依赖注入的方式使用
+        return cachePenetrationBloomFilter;
+    }
+}
