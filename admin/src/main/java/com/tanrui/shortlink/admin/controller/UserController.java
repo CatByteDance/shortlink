7d62b7b3b5ceff34dda7443d7ed3aad27c6c7b12
tan rui <tan870856@gamil.com>
2024-11-05 23:56:54 +0800

    feature:控制层 用户登录和检查用户是否登录

--- admin/src/main/java/com/tanrui/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/tanrui/shortlink/admin/controller/UserController.java
@@ -4,9 +4,11 @@ package com.tanrui.shortlink.admin.controller;
 import cn.hutool.core.bean.BeanUtil;
 import com.tanrui.shortlink.admin.common.convention.result.Result;
 import com.tanrui.shortlink.admin.common.convention.result.Results;
+import com.tanrui.shortlink.admin.dto.req.UserLoginReqDTO;
 import com.tanrui.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.tanrui.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.tanrui.shortlink.admin.dto.resp.UserActualRespDTO;
+import com.tanrui.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.tanrui.shortlink.admin.dto.resp.UserRespDTO;
 import com.tanrui.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -83,6 +85,23 @@ public class UserController {
     public Result<Void> updateUser(@RequestBody UserUpdateReqDTO reqDTO) {
         userService.update(reqDTO);
         return Results.success();
+    }
+
+
+    /**
+     * 用户登录
+     */
+    @PostMapping("/api/short-link/v1/user/login")
+    public Result<UserLoginRespDTO> login(@RequestBody UserLoginReqDTO reqDTO) {
+        return Results.success(userService.login(reqDTO));
+    }
+
+    /**
+     * 检查用户是否登录
+     */
+    @GetMapping("/api/short-link/v1/user/check-login")
+    public Result<Boolean> checkLogin(@RequestParam("username") String username, @RequestParam("token") String token) {
+        return Results.success(userService.checkLogin(username,token));
     }
 }
 
