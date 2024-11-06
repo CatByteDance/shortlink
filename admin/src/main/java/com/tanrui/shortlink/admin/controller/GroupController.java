6356740735b44ce3fb2abf40b1f90aee6453aa77
tan rui <tan870856@gamil.com>
2024-11-06 15:55:52 +0800

    feature: 开发短链接后管新增分组功能

--- admin/src/main/java/com/tanrui/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/tanrui/shortlink/admin/controller/GroupController.java
@@ -1,7 +1,12 @@
 package com.tanrui.shortlink.admin.controller;
 
+import com.tanrui.shortlink.admin.common.convention.result.Result;
+import com.tanrui.shortlink.admin.common.convention.result.Results;
+import com.tanrui.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
 import com.tanrui.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
         private final GroupService groupService;
 
+        @PostMapping("/api/short-link/v1/group")
+        public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO shortLinkGroupSaveReqDTO) {
+                groupService.saveGroup(shortLinkGroupSaveReqDTO.getName());
+                return Results.success();
+        }
 }
--- admin/src/main/java/com/tanrui/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/tanrui/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,9 +1,12 @@
 package com.tanrui.shortlink.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tanrui.shortlink.admin.dao.entity.GroupDO;
 import com.tanrui.shortlink.admin.dao.mapper.GroupMapper;
 import com.tanrui.shortlink.admin.service.GroupService;
+import com.tanrui.shortlink.admin.toolkit.RandomGenerator;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +18,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
 
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomGenerator.generateRandom();
+        } while (!hasGid(gid));
+
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .sortOrder(0)
+                .name(groupName)
+                .build();
+
+        baseMapper.insert(groupDO);
+    }
+
+    private boolean hasGid(String gid) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                // TODO 设置用户名
+                .eq(GroupDO::getUsername, null );
+
+                GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+                return hasGroupFlag == null;
+    }
 }
