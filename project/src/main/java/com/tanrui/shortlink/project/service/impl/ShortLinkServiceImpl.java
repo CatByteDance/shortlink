293e4eb71444c8c4071e0053009b5edf7b353292
tan rui <tan870856@gamil.com>
2024-11-09 19:12:46 +0800

    feature: 修改相似原始链接大量并发场景下生成冲突问题

--- project/src/main/java/com/tanrui/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/tanrui/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,6 +1,6 @@
 package com.tanrui.shortlink.project.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.text.StrBuilder;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tanrui.shortlink.project.common.convention.exception.ServiceException;
 import com.tanrui.shortlink.project.dao.entity.ShortLinkDO;
@@ -15,6 +15,7 @@ import org.redisson.api.RBloomFilter;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 
 
 /**
@@ -30,22 +31,33 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO reqDTO) {
         String shortLinkSuffix = generateSuffix(reqDTO);
-        String fullShortUrl = reqDTO.getDomain() + "/" + shortLinkSuffix;
 
-        ShortLinkDO shortLinkDo = BeanUtil.toBean(reqDTO, ShortLinkDO.class);
-        shortLinkDo.setShortUri(shortLinkSuffix);
-        shortLinkDo.setEnableStatus(0);
-        shortLinkDo.setFullShortUrl(fullShortUrl);
+        String fullShortUrl = StrBuilder.create(reqDTO.getDomain())
+                .append("/")
+                .append(shortLinkSuffix)
+                .toString();
 
-//      解决布隆过滤器出现误判的情况
+        ShortLinkDO shortLinkDo = ShortLinkDO.builder()
+                .domain(reqDTO.getDomain())
+                .originUrl(reqDTO.getOriginUrl())
+                .gid(reqDTO.getGid())
+                .createdType(reqDTO.getCreatedType())
+                .validDateType(reqDTO.getValidDateType())
+                .validDate(reqDTO.getValidDate())
+                .describe(reqDTO.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .fullShortUrl(fullShortUrl)
+                .build();
+
+        /**
+         * 唯一索引的判断，高并发的场景下如果两个相同的短链接请求插入，只有一个能成功，另一个抛出异常
+         * 如果使用分布式锁解决的话，性能会得到降低
+         */
         try{
             baseMapper.insert(shortLinkDo);
         } catch (DuplicateKeyException e){
-//          TODO 已经判误的短链接如何处理
-//          第一种情况 短链接确实真实存在缓存中
-//          第二种情况 短链接不一定存在缓存中
-            log.warn("短链接 {} 重复入库", fullShortUrl);
-            throw new ServiceException("短链接生成重复");
+            throw new ServiceException(String.format("短链接：%s 生成重复", fullShortUrl));
         }
 
 //      检查无重复之后，存入布隆过滤器当中
@@ -62,15 +74,15 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
         int customGenerateCount = 0;
         String shortLinkSuffix;
 
-//      TODO: 没太明白这里的循环是干嘛, 存疑：如果originUrl没发生改变的化，
-//       每次生成的shortLinkSuffix都是相同的，个人认为应该在此处实现每次循环都得到不同的shortLinkSuffix
         while (true){
             if (customGenerateCount > 10){
                 throw new ServiceException("短链接频繁生成，请稍后再试");
             }
 
-//          只要OriginUrl没有改变，生成的shortLinkSuffix也不会改变，为什么要重复？
             String originUrl = reqDTO.getOriginUrl();
+
+//          改变originUrl,避免冲突
+            originUrl += UUID.randomUUID().toString();
             shortLinkSuffix = HashUtil.hashToBase62(originUrl);
 
 //          判断不存在，则一定不存在, 返回false，然后break
